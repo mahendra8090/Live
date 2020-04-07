f8bafcec594663e404e48318059d76e5b71a08d2
mahendra kumar <you@example.com>
2020-04-08 01:58:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/live/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.live;
+
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.TextView;
+
+import com.bambuser.broadcaster.BroadcastPlayer;
+import com.bambuser.broadcaster.PlayerState;
+import com.bambuser.broadcaster.SurfaceViewWithAutoAR;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    SurfaceViewWithAutoAR mVideoSurface;
+    TextView mPlayerStatusTextView;
+    View mPlayerContentView;
+    final OkHttpClient mOkHttpClient = new OkHttpClient();
+    BroadcastPlayer mBroadcastPlayer;
+    MediaController mMediaController = null;
+    private static final String APPLICATION_ID = "xPknDMqYsYAtal3IpvCOmQ";
+    private static final String API_KEY = "WSAoRiQahJozbo7zjUV1gu";
+    Display mDefaultDisplay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVideoSurface = findViewById(R.id.VideoSurfaceView);
+        mPlayerStatusTextView = findViewById(R.id.PlayerStatusTextView);
+        mPlayerContentView = findViewById(R.id.PlayerContentView);
+        mDefaultDisplay = getWindowManager().getDefaultDisplay();
+    }
+    BroadcastPlayer.Observer mBroadcastPlayerObserver = new BroadcastPlayer.Observer() {
+        @Override
+        public void onStateChange(PlayerState playerState) {
+            if (mPlayerStatusTextView != null)
+                mPlayerStatusTextView.setText("Status: " + playerState);
+            if (playerState == PlayerState.PLAYING || playerState == PlayerState.PAUSED || playerState == PlayerState.COMPLETED) {
+                if (mMediaController == null && mBroadcastPlayer != null && !mBroadcastPlayer.isTypeLive()) {
+                    mMediaController = new MediaController(MainActivity.this);
+                    mMediaController.setAnchorView(mPlayerContentView);
+                    mMediaController.setMediaPlayer(mBroadcastPlayer);
+                }
+                if (mMediaController != null) {
+                    mMediaController.setEnabled(true);
+                    mMediaController.show();
+                }
+            } else if (playerState == PlayerState.ERROR || playerState == PlayerState.CLOSED) {
+                if (mMediaController != null) {
+                    mMediaController.setEnabled(false);
+                    mMediaController.hide();
+                }
+                mMediaController = null;
+            }
+        }
+        @Override
+        public void onBroadcastLoaded(boolean live, int width, int height) {
+            Point size = getScreenSize();
+            float screenAR = size.x / (float) size.y;
+            float videoAR = width / (float) height;
+            float arDiff = screenAR - videoAR;
+            mVideoSurface.setCropToParent(Math.abs(arDiff) < 0.2);
+        }
+    };
+    private Point getScreenSize() {
+        if (mDefaultDisplay == null)
+            mDefaultDisplay = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        try {
+            // this is officially supported since SDK 17 and said to work down to SDK 14 through reflection,
+            // so it might be everything we need.
+            mDefaultDisplay.getClass().getMethod("getRealSize", Point.class).invoke(mDefaultDisplay, size);
+        } catch (Exception e) {
+            // fallback to approximate size.
+            mDefaultDisplay.getSize(size);
+        }
+        return size;
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mOkHttpClient.dispatcher().cancelAll();
+        mVideoSurface = null;
+        if (mBroadcastPlayer != null)
+            mBroadcastPlayer.close();
+        mBroadcastPlayer = null;
+        if (mMediaController != null)
+            mMediaController.hide();
+        mMediaController = null;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (ev.getActionMasked() == MotionEvent.ACTION_UP && mBroadcastPlayer != null && mMediaController != null) {
+            PlayerState state = mBroadcastPlayer.getState();
+            if (state == PlayerState.PLAYING ||
+                    state == PlayerState.BUFFERING ||
+                    state == PlayerState.PAUSED ||
+                    state == PlayerState.COMPLETED) {
+                if (mMediaController.isShowing())
+                    mMediaController.hide();
+                else
+                    mMediaController.show();
+            } else {
+                mMediaController.hide();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVideoSurface = findViewById(R.id.VideoSurfaceView);
+        mPlayerStatusTextView.setText("Loading latest broadcast");
+        getLatestResourceUri();
+    }
+
+    void getLatestResourceUri() {
+        Request request = new Request.Builder()
+                .url("https://api.bambuser.com/broadcasts")
+                .addHeader("Accept", "application/vnd.bambuser.v1+json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + API_KEY)
+                .get()
+                .build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                runOnUiThread(new Runnable() { @Override public void run() {
+                    if (mPlayerStatusTextView != null)
+                        mPlayerStatusTextView.setText("Http exception: " + e);
+                }});
+            }
+            @Override
+            public void onResponse(final Call call, final Response response) throws IOException {
+                String body = response.body().string();
+                String resourceUri = null;
+                try {
+                    JSONObject json = new JSONObject(body);
+                    JSONArray results = json.getJSONArray("results");
+                    JSONObject latestBroadcast = results.optJSONObject(0);
+                    resourceUri = latestBroadcast.optString("resourceUri");
+                } catch (Exception ignored) {}
+                final String uri = resourceUri;
+                runOnUiThread(new Runnable() { @Override public void run() {
+                    initPlayer("https://cdn.bambuser.net/broadcasts/2667d975-4e2e-4726-ac31-5f086897c00f?da_signature_method=HMAC-SHA256&da_id=9e1b1e83-657d-7c83-b8e7-0b782ac9543a&da_timestamp=1585315973&da_static=1&da_ttl=0&da_signature=ad67c08bfa2852524691f6e38fdb6c94ff7f92acf9914ee7e84a60434bce7d5f");
+                }});
+            }
+        });
+    }
+
+    void initPlayer(String resourceUri) {
+        if (resourceUri == null) {
+            if (mPlayerStatusTextView != null)
+                mPlayerStatusTextView.setText("Could not get info about latest broadcast");
+            return;
+        }
+        if (mVideoSurface == null) {
+            // UI no longer active
+            return;
+        }
+        if (mBroadcastPlayer != null)
+            mBroadcastPlayer.close();
+        mBroadcastPlayer = new BroadcastPlayer(this, resourceUri, APPLICATION_ID, mBroadcastPlayerObserver);
+        mBroadcastPlayer.setSurfaceView(mVideoSurface);
+        mBroadcastPlayer.load();
+    }
+    // ...
+
+
+}
